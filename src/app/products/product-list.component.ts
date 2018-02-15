97270cf04360240647f2a6995a8e635ff5b84122
gchadzynski <gorinoko@gmail.com>
2018-02-15 23:26:55 +0100

    More on Components

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 
 @Component({
@@ -6,13 +6,22 @@ import { IProduct } from "./product";
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.components.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List !';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = "cart";
 
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string){
+        this._listFilter = value;
+        this.filerProducts = this.listFilter ? this.preformFilter(this.listFilter) : this.products;
+    }
+
+    filerProducts : IProduct[];
     products: IProduct[] = [
         {
             "productId": 1,
@@ -36,7 +45,22 @@ export class ProductListComponent {
         }
     ];
 
+    constructor() {
+        this.filerProducts = this.products;
+        this.listFilter = 'cart'
+    }
+
     toggleImage() : void{
         this.showImage = !this.showImage;
+    }
+
+    preformFilter(filterBy: string) : IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => 
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
+    ngOnInit(): void {
+        console.log('Init')
     }
 }
